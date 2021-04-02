7ba8eccba3f5af3d6c4b7103613a516dd1975e70
Brock <brockmck2003@gmail.com>
2021-04-01 18:33:51 -0700

    added a button that can add search engines

---
+++ search.js
@@ -0,0 +1,10 @@
+function addInput() {
+    var newdiv = document.createElement('div');
+    //newdiv.id = dynamicInput[counter];
+    newdiv.innerHTML = "Entry <br><input type='text' name='myInputs[]' placeholder = 'https://example.com/search?q={searchterms}}'> <input type='button' value='-' onClick='removeInput(this);'>";
+    document.getElementById('formulario').appendChild(newdiv);
+}
+
+function removeInput(btn) {
+    btn.parentNode.remove();
+}
