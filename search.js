b591d245fdb1cc9c4574e7b2b5223aa0735c8255
Brock <brockmck2003@gmail.com>
2021-04-01 23:56:21 -0700

    readability

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 function addInput() {
     var newdiv = document.createElement('div');
     //newdiv.id = dynamicInput[counter];
-    newdiv.innerHTML = "Entry <br><input type='text' name='myInputs[]' placeholder = 'https://example.com/search?q={searchterms}}'> <input type='button' value='-' onClick='removeInput(this);'>";
+    newdiv.innerHTML = "Entry <br><input type='text' name='myInputs[]' placeholder = 'https://example.com/search?q={searchterms}}' size = '45'> <input type='button' value='-' onClick='removeInput(this);'>";
     document.getElementById('formulario').appendChild(newdiv);
 }
 
