bc4cb64433a6969f856aeec57534dcc0016917ae
Brock <brockmck2003@gmail.com>
2021-04-04 21:45:28 -0700

    transitioning to chrome extension

--- background.js
+++ background.js
@@ -7,19 +7,55 @@
 // * addListener - what we want to do with this event
 //
 // See https://developer.chrome.com/docs/extensions/reference/events/ for additional details.
-chrome.runtime.onInstalled.addListener(async) => {
 
-    // While we could have used `let url = "hello.html"`, using runtime.getURL is a bit more robust as
-    // it returns a full URL rather than just a path that Chrome needs to be resolved contextually at
-    // runtime.
-    let url = chrome.runtime.getURL("front.ejs");
+$(document).ready(function() {
 
-    // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
-    //
-    // Many of the extension platform's APIs are asynchronous and can either take a callback argument
-    // or return a promise. Since we're inside an async function, we can await the resolution of the
-    // promise returned by the tabs.create call. See the following link for more info on async/await.
-    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
-    let tab = await chrome.tabs.create({ url })
-};
+    function addInput() {
+        var newdiv = document.createElement('div');
+        //newdiv.id = dynamicInput[counter];
+        newdiv.innerHTML = "<br><input type='text' name='myInputs' id ='custom' placeholder = 'https://example.com/search?q=' size = '45' style = 'margin-left: 3%;'> <input type='button' value='-' onClick='removeInput(this);'>";
+        document.getElementById('myForm').appendChild(newdiv);
+    }
+
+    function removeInput(btn) {
+        btn.parentNode.remove();
+
+    }
+
+    function submitTerm() {
+        var input = document.getElementsByClassName('searchTerm').value;
+        console.log(input)
+    }
+
+
+
+
+
+
+    function submitInput() {
+
+
+        let input = document.getElementById('searchTerm').value;
+
+        $(document).ready(function() {
+            $('input[type="checkbox"]:checked').each(function() { // $(':checkbox:checked')
+                window.open(this.value + input);
+                window.location.reload(); // $(this).val()
+            });
+        });
+    }
+
+
+    function submitCustom() {
+        let input = document.getElementById('searchTerm').value;
+
+
+        $(document).ready(function() {
+            $('input[id="custom"]').each(function() { // $(':checkbox:checked')
+                window.open(this.value + input);
+                window.location.reload(); // $(this).val()
+            });
+        });
+    }
+
+});
