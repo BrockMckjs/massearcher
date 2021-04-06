bd7eb0bb0fe24ce4cf86fcbb6e9ff61ec0c0760d
Brock <brockmck2003@gmail.com>
2021-04-06 01:37:58 -0700

    fixed custom searches

--- search.js
+++ search.js
@@ -1,5 +1,4 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
-
 document.addEventListener('DOMContentLoaded', function() {
     var buttonSub = document.getElementById('select');
     var input = document.getElementById('searchTerm').value;
@@ -8,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     buttonSub.addEventListener('click', function() {
         $(document).ready(function() {
             $('input[id="customInput"]').each(function() { // $(':checkbox:checked')
-                chrome.tabs.create({ url: customValue.value + document.getElementById('searchTerm').value });
+                chrome.tabs.create({ url: document.getElementById('customInput').value + document.getElementById('searchTerm').value });
                 window.location.reload();
             })
         })
@@ -34,22 +33,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    $(document).ready(function() {
-
-        var addButton = document.getElementById('custom')
-        addButton.addEventListener('click', function() {
+    var addButton = document.getElementById('custom')
+    addButton.addEventListener('click', function() {
+        $(document).ready(function() {
             var newdiv = document.createElement('div');
             //newdiv.id = dynamicInput[counter];
-            newdiv.innerHTML = "<br><input type='text' name='myInputs' id='customInput' placeholder='https://example.com/search?q=' size='45' style='margin-left: 3%;width: 190px;'>" // add a remove button (messes up styling on front.html, will fix in later update.) <input type='button' value='-' id='removeInput'>";
+            newdiv.innerHTML = "<input type='text' name='myInputs' id='customInput' placeholder='https://example.com/search?q=' size='45' style='margin-left: 3%;width: 190px;'>" // add a remove button (messes up styling on front.html, will fix in later update.) <input type='button' value='-' id='removeInput'>";
             document.getElementById('myForm').appendChild(newdiv);
 
-            //    document.addEventListener('click', function() {
-
-
-            //var removeInput = document.getElementById('removeInput')
-            //removeInput.addEventListener('click', function() {
-            //  btn.parentNode.remove();
-            // ^^^^ this is all remove cutsom input button functionality. didnt add it yet because it broke the extension, but works on regular html. need to make it work with external js file.
         })
+
+        //    document.addEventListener('click', function() {
+
+
+        //var removeInput = document.getElementById('removeInput')
+        //removeInput.addEventListener('click', function() {
+        //  btn.parentNode.remove();
+        // ^^^^ this is all remove cutsom input button functionality. didnt add it yet because it broke the extension, but works on regular html. need to make it work with external js file.
     })
 })
