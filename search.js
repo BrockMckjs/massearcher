f557f388111b5c350c672cbe20cf2d4bc7c96272
Brock <brockmck2003@gmail.com>
2021-04-06 00:58:32 -0700

    getting custom input to work

--- search.js
+++ search.js
@@ -1,84 +1,14 @@
-window.onload = function() {
-
-    var buttonSub = document.getElementById("select")
-
-    if (buttonSub) {
-        (buttonSub.addEventListener('click,', submitInput(), submitCustom()))
-    }
-}
-
-function addInput() {
-    var newdiv = document.createElement('div');
-    //newdiv.id = dynamicInput[counter];
-    newdiv.innerHTML = "<br><input type='text' name='myInputs' id ='custom' placeholder = 'https://example.com/search?q=' size = '45' style = 'margin-left: 3%;'> <input type='button' value='-' id='removeInput'>";
-    document.getElementById('myForm').appendChild(newdiv);
-}
-
-function removeInput(btn) {
-    btn.parentNode.remove();
-
-}
-
-function submitTerm() {
-    var input = document.getElementsByClassName('searchTerm').value;
-    console.log(input)
-}
-
-
-
-
-
-function submitInput() {
-
-
-    let input = document.getElementById('searchTerm').value;
-
-    $(document).ready(function() {
-        $('input[type="checkbox"]:checked').each(function() { // $(':checkbox:checked')
-            tabs.create(this.value + input);
-            window.location.reload(); // $(this).val()
-        });
-    });
-}
-
-
-function submitCustom() {
-    let input = document.getElementById('searchTerm').value;
-
-
-    $(document).ready(function() {
-        $('input[id="custom"]').each(function() { // $(':checkbox:checked')
-            tabs.create(this.value + input);
-            window.location.reload(); // $(this).val()
-        });
-    });
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var buttonSub = document.getElementById('select');
-    let input = document.getElementById('searchTerm').value;
-    //onclick
-    buttonSub.addEventListener('click', function() {
-        $(document).ready(function() {
-            $('input[id="custom"]').each(function() { // $(':checkbox:checked')
-                tabs.create(this.value + input);
-                window.location.reload();
-            })
-        })
-    })
-})
-
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener('DOMContentLoaded', function() {
     var buttonSub = document.getElementById('select');
     var input = document.getElementById('searchTerm').value;
-    //onclick
+    let customValue = document.getElementById('customInput').value
+        //onclick
     buttonSub.addEventListener('click', function() {
         $(document).ready(function() {
-            $('input[id="custom"]').each(function() { // $(':checkbox:checked')
-                chrome.tabs.create(this.value + input);
+            $('input[id="customInput"]').each(function() { // $(':checkbox:checked')
+                chrome.tabs.create({ url: customValue + document.getElementById('searchTerm').value });
                 window.location.reload();
             })
         })
@@ -89,8 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var buttonSub = document.getElementById('select');
     var input = document.getElementById('searchTerm').value;
-    var value = document.getElementsBy
-        //onclick
+    //onclick
     buttonSub.addEventListener('click', function() {
         $(document).ready(function() {
             $('input[type="checkbox"]:checked').each(function() { // $(':checkbox:checked')
@@ -100,3 +29,40 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     })
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    var addButton = document.getElementById('custom')
+    addButton.addEventListener('click', function() {
+        var newdiv = document.createElement('div');
+        //newdiv.id = dynamicInput[counter];
+        newdiv.innerHTML = "<br><input type='text' name='myInputs' id='customInput' placeholder='https://example.com/search?q=' size='45' style='margin-left: 3%;width: 190px;'>" // add a remove button (messes up styling on front.html, will fix in later update.) <input type='button' value='-' id='removeInput'>";
+        document.getElementById('myForm').appendChild(newdiv);
+
+        document.addEventListener('click', function() {
+
+
+            //var removeInput = document.getElementById('removeInput')
+            //removeInput.addEventListener('click', function() {
+            //  btn.parentNode.remove();
+            // ^^^^ this is all remove cutsom input button functionality. didnt add it yet because it broke the extension, but works on regular html. need to make it work with external js file.
+        })
+    })
+})
+
+
+//document.addEventListener('click', function() {
+//  var buttonSub = document.getElementById('select');
+//var input = document.getElementById('searchTerm').value;
+//let values = document.getElementById('customInput').value
+
+//buttonSub.addEventListener('click', function() {
+//  document.getElementById('customInput').each(function() {
+//    chrome.tabs.create({ url: values + document.getElementById('searchTerm').value });
+//  window.location.reload();
+// })
+//})
+//})
