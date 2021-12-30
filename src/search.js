34c9d0f63df6adc6966213c04a050b895c6cbb06
jdflyer <jdflyer10@gmail.com>
2021-12-29 19:25:03 -0700

    Fix code

--- src/search.js
+++ src/search.js
@@ -1,44 +1,33 @@
 //submit functions
 
-//submit custom
-function submitA() {
+//submit custom 
+
+function submit() {
     $(document).ready(function() {
         $('input[id="customInput"]').each(function() { // $(':checkbox:checked')
             chrome.tabs.create({ url: document.getElementById('customInput').value + document.getElementById('searchTerm').value });
             window.location.reload();
-})
-})
-}
-
-
-//submit checked
-function submitB() {
+        })
+    })
     $(document).ready(function() {
         $('input[type="checkbox"]:checked').each(function() { // $(':checkbox:checked')
             chrome.tabs.create({ url: this.value + document.getElementById('searchTerm').value });
             window.location.reload();
-})
-})
+        })
+    })
 }
-
-
-// in a future update, these functions should be merged together and just have values passed through them, as they are nearly identical. but since i am lazy and working on a piece of software that nobody but me will use, i do not care.
-
 
 ///submit searches
 document.addEventListener('DOMContentLoaded', function() {
     var buttonSub = document.getElementById('select');
-        //onclick
     buttonSub.addEventListener('click', function() {
-        submitA();
-        submitB();
-            })
-        })
+        submit();
+    })
+})
 
 
 /// add searches
 document.addEventListener('DOMContentLoaded', function() {
-
     var addButton = document.getElementById('custom')
     addButton.addEventListener('click', function() {
         $(document).ready(function() {
@@ -56,8 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var input = document.getElementById('searchTerm');
     input.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
-            submitA();
-            submitB();
+            submit();
         }
     })
 })
